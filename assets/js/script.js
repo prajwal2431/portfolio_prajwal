08db53db56b5dbe3fec0828a6538255ceeb80e2d
prajwal2431 <prajwalnivangune914@gmail.com>
2024-11-18 11:02:43 +0530

    some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,7 +85,7 @@ var typed = new Typed(".typing-text", {
 async function fetchData(type = "skills") {
     let response
     type === "skills" ?
-        response = await fetch("skills.json")
+        response = await fetch("./skills.json")
         :
         response = await fetch("./projects/projects.json")
     const data = await response.json();
